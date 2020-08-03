3a46ed4dea43f1f57bc734f24986a7338691c16f
Jesus <jsuesalnugo@gmail.com>
2020-08-03 17:54:54 -0500

    prueba

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/my-project/'
+  : '/',
     title: 'PanaUpgrade',
     description: 'Just playing around',
     themeConfig: {
