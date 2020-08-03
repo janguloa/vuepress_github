918ef218eafb8ec1854cb87a47d842292ffa9632
Jesus <jsuesalnugo@gmail.com>
2020-08-03 17:10:02 -0500

    prueba

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ module.exports = {
     description: 'Just playing around',
     themeConfig: {
         nav: [
-          { text: 'Inicio', link: '/' },
+          { text: 'Home', link: '/' },
           { text: 'Guía', link: '/guia/' },
           { text: 'Canal youtube', link: 'https://www.youtube.com/watch?v=zk6BwgxnBtE' }
         ],
